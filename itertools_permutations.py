4bf6bf5a4df56be7d0a9f258c154669a8c71c401
MasumTech <masumrezadiu@gmail.com>
2020-02-12 02:47:14 +0600

    Solution of itertools.permutations() in Python

---
+++ itertools_permutations.py
@@ -0,0 +1,31 @@
+from itertools import permutations
+string = input().split(' ')
+
+str1 = string[0]
+s1 = []
+for i in range(len(str1)):
+    s1.append(str1[i])
+s1.sort()
+# print(s1)
+e = int(string[1])
+
+if e == 1:
+    for i in range(len(s1)):
+        print(s1[i])
+else:
+    li = list(permutations(s1, e))
+
+    # print(str(li))
+
+    li1 = []
+
+    for i in range(len(li)):
+        lttr = ''
+        for j in range(e):
+            lttr = lttr + li[i][j]
+        li1.append(lttr)
+    li1.sort()
+    #print(li1)
+
+    for i in range(len(li1)):
+        print(li1[i])
